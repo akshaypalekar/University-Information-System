99fac828c2c7e29775028b44592c5417c6026cbf
akshaypalekar <akshaypalekar91@gmail.com>
2017-12-03 17:47:55 -0500

    Navigation bar on Homepage

--- Homepage.php
+++ Homepage.php
@@ -4,10 +4,16 @@ session_start();
 ?>
 <html>
 <title>University Information System</title>
-<head></head>
+<head>
+<link rel="stylesheet" type="text/css" href="css.css">
+</head>
 <body>
-    <div class="sidebar_container">
-
+    <div id="Sidenav_container" class="sidenav_container">
+    <a href="#">Search</a>
+    <a href="#">Accounts</a>
+    <a href="#">Blackboard</a>
+    <a href="#">Career Services</a>
+    <a href="#">Personal Information</a>
     </div>
 </body>
 </html>
