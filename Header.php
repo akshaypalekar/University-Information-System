2db2d5fd597b86866bd27f46dd8cf3fcc49800d9
akshaypalekar <akshaypalekar91@gmail.com>
2017-12-03 19:49:30 -0500

    Adding header

---
+++ Header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="css.css">
+    </head>
+    <body>
+        <div class="header_container"></div>
+    </body>
+</html>
--- Homepage.php
+++ Homepage.php
@@ -5,6 +5,7 @@ session_start();
 <html>
 <title>University Information System</title>
 <head>
+<?php include 'Header.php' ?>
 <?php include 'Sidebar.php' ?>
 <link rel="stylesheet" type="text/css" href="css.css">
 </head>
