40cb3f570dc23dd7a1d60969c10e0e136f1306fc
poojamanjeshwar <poojamanjeshwar1992@gmail.com>
2017-12-03 19:48:44 -0500

    Login page css added

--- Login_page.php
+++ Login_page.php
@@ -4,8 +4,11 @@ session_start();
 ?>
 <html>
 <title>Computer Science Dept</title>
-<head></head>
-<body background="Images/nyit_bg.jpg" style ="background-size: 100%;">
+<head>
+<link rel="stylesheet" type="text/css" href="css.css">
+</head>
+
+<body background="Images/nyit_bg.jpg">
     <?php
     $user_invalid=@$_SESSION['user_invalid'];
     if($user_invalid)
@@ -15,18 +18,22 @@ session_start();
     if(isset($_SESSION['user_invalid']))
     unset($_SESSION['user_invalid']);
     ?>
+ <div class="form">
+ <div class="thumbnail"><img src="Images/nyit_loginbox.jpg"></div>
     <form method="post" action="Login_authentication.php">
         <div class="Login_container">
-            <label>
+            <!-- <label>
                 <b>Username</b>
-            </label>
-            <input type="text" placeholder="Enter Username" name="username" id="username" required>
-            <label>
+            </label> -->
+            <input type = "text" placeholder="Enter Username" name="username" id="username" required>
+            <!-- <label>
                 <b>Password</b>
-            </label>
-            <input type="password" placeholder="Enter Password" name="pwd" id="pwd" required>
-            <input type="submit" value="Login">
+            </label> -->
+            <input type = "password" placeholder="Enter Password" name="pwd" id="pwd" required>
+            <!-- <input type = "submit" value="Login"> -->
+            <button type="submit">Login</button>
         </div>
     </form>
+</div>
 </body>
 </html>
