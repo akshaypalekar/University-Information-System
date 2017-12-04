f0cebbbce29408a147dd003746e617150f191167
akshaypalekar <akshaypalekar91@gmail.com>
2017-12-04 11:35:12 -0500

    Making sidebar items span

--- Sidebar.php
+++ Sidebar.php
@@ -6,12 +6,13 @@
 <body>
     <div id="Sidenav_container" class="sidenav_container">
     <a href="#"><img src="Images/nyit_logo.jpg" height="130" width="200"/></a>
-    <a href="#">Search</a>
-    <a href="#">Accounts</a>
-    <a href="#">Blackboard</a>
-    <a href="#">Career Services</a>
-    <a href="#">Personal Information</a>
-    <a href="Logout.php">Logout</a>
+    <span class="nav_items"><a href="#">Home</a></span>
+    <span class="nav_items"><a href="#">Search</a></span>
+    <span class="nav_items"><a href="#">Accounts</a></span>
+    <span class="nav_items"><a href="#">Blackboard</a></span>
+    <span class="nav_items"><a href="#">Career Services</a></span>
+    <span class="nav_items"><a href="#">Personal Information</a></span>
+    <span class="nav_items"><a href="Logout.php">Logout</a></span>
     </div>
 </body>
 </html>
